51269b6052f7c7442e1e7a68a29d63d39e62ba4b
Tolik <t1005828@gmail.com>
2024-08-02 18:11:02 +0300

    fixed code

--- app/src/main/java/com/fermers_marketplace/fermers/presentation/fragments/home/screens_home/RecommendationFragment.kt
+++ app/src/main/java/com/fermers_marketplace/fermers/presentation/fragments/home/screens_home/RecommendationFragment.kt
@@ -6,10 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
+import com.fermers_marketplace.common.ScrollToTopListener
 import com.fermers_marketplace.fermers.R
 import com.fermers_marketplace.fermers.data.model.AdvertModel
 import com.fermers_marketplace.fermers.databinding.FragmentRecommendationBinding
-import com.fermers_marketplace.common.ScrollToTopListener
 import com.fermers_marketplace.fermers.presentation.fragments.chosen.adapter.AdvertAdapter
 
 class RecommendationFragment : Fragment(), ScrollToTopListener {
@@ -50,14 +50,11 @@ class RecommendationFragment : Fragment(), ScrollToTopListener {
             AdvertModel(R.drawable.horse, "Продаю лошадь", "Цена 120тыс"),
             AdvertModel(R.drawable.cow, "Продаю корову", "Цена 80тыс"),
             AdvertModel(R.drawable.ram, "Продаю барана", "Цена 50тыс"),
-
-            )
+        )
 
         val adapter = AdvertAdapter(items)
         binding.rvBuyHome.adapter = adapter
         binding.rvBuyHome.layoutManager = GridLayoutManager(requireContext(), 2)
-        binding.rvBuyHome.adapter = adapter
-
     }
 
     override fun scrollToTop() {
