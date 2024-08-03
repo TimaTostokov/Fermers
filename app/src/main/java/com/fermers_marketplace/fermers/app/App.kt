3cc9ff57056eadffa54b33be74d94dc4adc43284
DosmirK <Dosmir017@gmail.com>
2024-08-03 20:29:55 +0600

    added profile

--- app/src/main/java/com/fermers_marketplace/fermers/app/App.kt
+++ app/src/main/java/com/fermers_marketplace/fermers/app/App.kt
@@ -1,7 +1,17 @@
 package com.fermers_marketplace.fermers.app
 
 import android.app.Application
+import com.fermers_marketplace.fermers.utils.pref.ProfileSharedPreferences
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application(){
+
+    var mySharedPreferences: ProfileSharedPreferences? = null
+
+    override fun onCreate() {
+        super.onCreate()
+
+        mySharedPreferences = ProfileSharedPreferences(this)
+    }
+}
--- app/src/main/java/com/fermers_marketplace/fermers/presentation/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/fermers_marketplace/fermers/presentation/fragments/profile/ProfileFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.fermers_marketplace.fermers.databinding.FragmentProfileBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
@@ -16,13 +18,7 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-    }
-
 }
